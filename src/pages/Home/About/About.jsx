67495c08902d75c0b572ea6a7518f9d610c8ee7a
joyato96 <joyato.chakma2025@gmail.com>
2023-10-25 23:27:54 +0600

    about section added

---
+++ src/pages/Home/About/About.jsx
@@ -0,0 +1,23 @@
+import person from '../../../../assets/images/about_us/person.jpg'
+import parts from '../../../../assets/images/about_us/parts.jpg'
+
+const About = () => {
+    return (
+        <div className="hero min-h-screen pb-10 bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className='lg:w-1/2 relative'>
+                    <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
+                    <img src={parts} className="w-1/2 right-5 top-1/2 absolute rounded-lg border-8 border-white shadow-2xl" />
+                </div>
+                <div className='lg:w-1/2 space-y-5 p-5'>
+                    <h3 className='text-3xl font-bold text-[#FF3811]'>About Us</h3>
+                    <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
+                    <p className='py-6 text-[#737373]'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. <br /> <br />The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <button className="p-2 pl-4 pr-4 rounded-md text-white bg-[#FF3811]">Get More Info</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -80,7 +80,7 @@ const Banner = () => {
                 <img src={img5} className="w-full rounded-xl" />
                 <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='text-white space-y-7 pl-12 w-1/2'>
-                        <h2 className='text-6xl'>Affordable Price For Car Servicing</h2>
+                        <h2 className='text-6xl font'>Affordable Price For Car Servicing</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
                             <button className='btn btn-primary mr-5'>Discover More</button>
