d3143135a79a69243ce79f2a10d0f600610e3405
joyato96 <joyato.chakma2025@gmail.com>
2023-10-26 02:42:52 +0600

    login and signup created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
     {
@@ -14,7 +15,11 @@ const router = createBrowserRouter([
         },
         {
           path:"/login",
-          element:<Login></Login>
+          element:<Login></Login>,
+        },
+        {
+          path:"/signup",
+          element:<SignUp></SignUp>
         }
       ]
     },
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,22 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import img from '../../../assets/images/login/login.svg'
-import { useContext } from 'react';
-import { AuthContext } from '../../providers/AuthProvider';
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
-const Login = () => {
-
-    const {signIn} = useContext(AuthContext);
+const SignUp = () => {
 
 
+    const {createUser} = useContext(AuthContext)
 
-    const handleLogin = e =>{
+
+    const handleSignUp = e =>{
         e.preventDefault()
         const form = e.target;
+        const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email,password)
+        console.log(name, email, password)
 
-        signIn(email, password)
+        createUser(email, password)
         .then(result =>{
             const user = result.user;
             console.log(user)
@@ -24,7 +25,6 @@ const Login = () => {
         .catch(error =>{
             console.error(error)
         })
-
     }
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -33,8 +33,14 @@ const Login = () => {
                     <img src={img} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={handleLogin} className="card-body">
-                        <h1 className='text-3xl text-center font-medium'>Login</h1>
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <h1 className='text-3xl text-center font-medium'>Sign Up</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-medium">Name</span>
+                            </label>
+                            <input type="email" name='name' placeholder="Your name" className="input input-bordered" required />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text font-medium">Email</span>
@@ -46,14 +52,11 @@ const Login = () => {
                                 <span className="label-text font-medium">Confirm Password</span>
                             </label>
                             <input type="password" name='password' placeholder="Your password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
                         </div>
                         <div className="form-control mt-6">
-                            <input className="p-2 text-white rounded-lg bg-[#FF3811]" type="submit" value="Sign In" />
+                            <input className="p-2 rounded-lg text-white bg-[#FF3811]" type="submit" value="Sign Up" />
                         </div>
-                        <p className='text-center'>Do not have an account? <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link></p>
+                        <p className='text-center'>Already have an account? <Link className='text-orange-600 font-bold' to="/login">Login</Link></p>
                     </form>
                     
                 </div>
@@ -62,4 +65,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
